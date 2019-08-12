e4d9456221434e11c747df41cfc5fc0a418046bb
srigan93 <43399167+srigan93@users.noreply.github.com>
2019-08-12 13:24:16 +0530

    Create excel_try.py

---
+++ excel_try.py
@@ -0,0 +1,14 @@
+''' before running this program make sure openpyxl is installed 
+how to install pip in fedora(install as root user) 
+# yum install python-pip
+# yum install epel-release(not mandatory)
+# pip install openpyxl '''
+
+import openpyxl as excel
+wb = excel.load_workbook('example.xlsx')
+print(type(wb))
+print(wb.get_sheet_names())
+sheet = wb.get_sheet_by_name(u'Sheet1')
+sheet['A1'] = 'Hello world!'
+print(sheet['A1'].value)
+~
